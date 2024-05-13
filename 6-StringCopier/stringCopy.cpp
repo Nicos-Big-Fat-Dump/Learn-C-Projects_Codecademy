f1137886069a3c4dcb03052d987c3ff380033b5c
Nico <lotailsmonsieur62c900@gmail.com>
2024-05-13 18:48:47 +0000

    added project 6

---
+++ 6-StringCopier/stringCopy.cpp
@@ -0,0 +1,21 @@
+//wait why was this a c++ file lol
+#include<stdio.h>
+#include<string.h>
+
+void copy(char* dst, char* src){
+    while(*src != '\0') {
+        *dst = *src;
+        src++;
+        dst++;
+    }
+    *dst = '\0'; 
+}
+
+int main(){
+    char srcString[] = "We promptly judged antique ivory buckles for the next prize!";
+
+    char dstString[strlen(srcString)];
+    copy(dstString, srcString);
+
+    printf("%s", dstString);
+}
